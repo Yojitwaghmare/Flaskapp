da5a0e64d4fab5ea64b1e3549fbd2386f350b510
Yojitwaghmare <yojeetwaghmare@gmail.com>
2025-12-13 05:01:29 +0530

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
 export DB_HOST="flaskapp-mydb-uzt9cdqml1vy.cruqqgmqsn5d.ap-south-1.rds.amazonaws.com"
 export DB_USER="admin"
 export DB_PASSWORD="password"
 export DB_NAME="dectobinary"
 
 cd /var/www/flaskapp
+
+# Create virtual environment if not exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
+
+pip install -r requirement.txt
+
 nohup python3 app.py > flask.log 2>&1 &
