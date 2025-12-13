5dba2cc5c7b166d9bb0f4c61e41d8854e486aa2f
Yojitwaghmare <yojeetwaghmare@gmail.com>
2025-12-13 18:32:13 +0530

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export DB_HOST="flaskapp-mydb-uzt9cdqml1vy.cruqqgmqsn5d.ap-south-1.rds.amazonaws.com"
-export DB_USER="admin"
-export DB_PASSWORD="password"
-export DB_NAME="dectobinary"
 
 cd /var/www/flaskapp
 
